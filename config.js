abb7bb6e1560b3658394c41a9436203cceb2cb4c
Vijay Ganesh H <vijay.g1@samsung.com>
2020-01-04 17:09:59 +0530

    Use direct mlab mongo for testing

--- config.js
+++ config.js
@@ -16,7 +16,8 @@ const Config = {
     bucket: 'fotoengel',
     region: 'us-west-2'
   },
-  mongoConnectionString: 'mongodb://localhost:27017/fotoengel'
+  mongoConnectionString: 'mongodb://localhost:27017/fotoengel',
+  mlab: "mongodb://heroku_msp29q92:jq2fe2r481ti5t0foufdisp40e@ds059471.mlab.com:59471/heroku_msp29q92",
 }
 
 module.exports = Config;
